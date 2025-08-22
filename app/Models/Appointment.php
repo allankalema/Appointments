4bf1a3b9a33f5ec1628ab7729187658796931240
allankalema4 <allankalema4@gmail.com>
2025-08-22 14:56:48 +0300

    i have created the  appointment mdel

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
+    /**
+     * Mass assignable fields.
+     */
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'details',
+        'appointment_date',
+        'worked_on',
+    ];
+
+    /**
+     * Appointment belongs to a patient.
+     */
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    /**
+     * Appointment belongs to a doctor.
+     */
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
 }
